4e9a186394aec768137b22800020f275adfb9fdf
JAYA PRAKASH SHRESTHA <jayaprakashshrestha15@gmail.com>
2023-05-11 14:48:26 -0500

    types of inheritance

---
+++ Day4/Dog.java
@@ -0,0 +1,20 @@
+import java.util.function.DoublePredicate;
+
+public class Dog{
+    // public - anywhere, everywhere
+    // private - within the class
+    // protected - package/folder
+
+    void name(String dname) {
+        System.out.println("Dog Name : " + dname);
+    } 
+    void price(int dprice) {
+        System.out.println("Dog Name : " + dprice);
+    } 
+    public static void main(String[] args) {
+        Dog puppy = new Dog();
+        puppy.name("German shepard");
+        puppy.price(100);
+    }
+    
+}
