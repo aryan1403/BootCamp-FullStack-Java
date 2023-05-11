24d27ea43888fd01ab7ddc89be95d07eebc04a63
JAYA PRAKASH SHRESTHA <jayaprakashshrestha15@gmail.com>
2023-05-11 14:27:09 -0500

    Inheritance Learned/Updated

--- Day4/inheritanceExample.java
+++ Day4/inheritanceExample.java
@@ -1,19 +1,15 @@
-public class inheritanceExample {
-    class Parent {
-        public static void test() {
+
+    class inheritanceExample {
+        public void test() {
             System.out.println("Hey");
         }
     }
 
-    class Parent2 {
-        void test() {
-            System.out.println("test2");
+
+    class Child extends inheritanceExample {
+        
+        public static void main(String[] args) {
+            new Child().test();
         }
     }
 
-    class Child extends Parent {
-        public static void main(String[] args) {
-            test();
-        }
-    }
-}
