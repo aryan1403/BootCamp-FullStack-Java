f7b8c35553f0af1300fbf7ebcd453c00b9a315f1
aryan1403 <aaryan14032006@gmail.com>
2023-05-12 01:35:45 +0530

    constructor Learned

---
+++ Day4/constructorExample.java
@@ -0,0 +1,9 @@
+public class constructorExample {
+    // constructor
+    public constructorExample() {
+        System.out.println("Running");
+    }
+    public static void main(String[] args) {
+        new constructorExample();
+    }
+}
