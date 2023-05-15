26dcf1d14ad5e9d27c7632e1df40338c9d3f0c7d
aryan1403 <aaryan14032006@gmail.com>
2023-05-15 23:46:17 +0530

    file handling done

---
+++ Day6/FileHandling.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class FileHandling {
+    public static void main(String[] args) {
+        File f = new File("demo.txt");
+        // Writing
+        try {
+            FileWriter fw = new FileWriter(f);
+            fw.append("We are good with Java ;)");
+            System.out.println("Appended Successfully");
+
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Reading
+        try {
+            // BufferedReader br = new BufferedReader();
+            Scanner sc = new Scanner(f);
+
+            while(sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+
+            sc.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }   
+}
