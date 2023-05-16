b6b9699387248ffc826b321cd5f9038076f7bd24
aryan1403 <aaryan14032006@gmail.com>
2023-05-17 00:39:29 +0530

    updated:

--- Day7/lambdaExample.java
+++ Day7/lambdaExample.java
@@ -1,20 +1,20 @@
 @FunctionalInterface
 interface in {
-    void work();
+    void work(String s);
 }
 
 class inex implements in {
     @Override
-    public void work() {
-        System.out.println("Working");
+    public void work(String s) {
+        System.out.println(s);
     }
 }
 public class lambdaExample {
     public static void main(String[] args) {
         in obj = new inex();
-        obj.work();
+        obj.work("it is working");
 
-        in obj2 = () -> System.out.println("it is working");
-        obj2.work();       
+        in obj2 = (s) -> System.out.println(s);
+        obj2.work("hey");       
     }
 }
