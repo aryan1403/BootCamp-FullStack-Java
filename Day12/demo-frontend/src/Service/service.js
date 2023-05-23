a638ffbee2e120f4381a9012fc5e4d4dc0865ac3
sushilkhanal <khanal.sushil47@gmail.com>
2023-05-23 15:00:11 -0500

    add item frontend

---
+++ Day12/demo-frontend/src/Service/service.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const URL = "http://localhost:8080/items";
+
+export function addItemToDB(name, purpose, cost, warranty) {
+  const data = {
+    name: name,
+    purpose: purpose,
+    cost: cost,
+    warranty: warranty,
+  };
+  axios
+    .post(URL + "/addItem", data)
+    .then((res) => {
+      console.log(res.data);
+      alert("Item added successfully!"); // Show success message
+      return res.data;
+    })
+    .catch((error) => {
+      console.error(error);
+      alert("Failed to add item."); // Show error message
+    });
+}
